f1d32d8f1f181dc9f57642b9e6dba4be761f4a53
AndrGrig <52404964+AndrGrig@users.noreply.github.com>
2019-07-17 11:51:00 +0300

    Update nodejs.groovy

--- basics/dsl/nodejs.groovy
+++ basics/dsl/nodejs.groovy
@@ -13,7 +13,15 @@ job('NodeJS example') {
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
     }
     steps {
-        shell("npm install")
+        dockerBuildAndPublish {
+            repositoryName(ac/dc)
+            tag('${GIT_REVISION,length=9}')
+            registryCredentials('dockerhub')
+            forcePull(false)
+            forceTag(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
     }
 }
 
